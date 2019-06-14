1176131dc5a80b358edf4f04d057dd040cc6b3bf
bcabanayan <bcabanayan@gmail.com>
2019-06-13 20:08:29 -0700

    Added the insert and bubble up functions to max heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -3,7 +3,11 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+    # add the value to the end of the array
+    self.storage.append(value)
+    # bubble it up
+    self._bubble_up(len(self.storage) - 1)
+
 
   def delete(self):
     pass
@@ -15,7 +19,19 @@ class Heap:
     pass
 
   def _bubble_up(self, index):
-    pass
+    # while at least one child node exists....(meaning there's a parent node)
+    while index > 0:
+      # find the parent index, using the parent index formula
+      parent = (index - 1)//2
+      # if the value at index is greater than the value at the parent index...
+      if self.storage[index] > self.storage[parent]:
+        # swap the values at each index...
+        self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+        # ...and then update the index of the value that was just swapped
+        index = parent
+      # otherwise, if value is not greater, then break the bubble up function
+      else:
+        break
 
   def _sift_down(self, index):
     pass
